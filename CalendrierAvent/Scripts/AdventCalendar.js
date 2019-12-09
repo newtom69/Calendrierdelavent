548a0e4201805c18f8efc0e8c1f755489e478280
newtom69 <thomas@vuille.fr>
2019-12-09 23:33:38 +0100

    continue

--- CalendrierAvent/Scripts/AdventCalendar.js
+++ CalendrierAvent/Scripts/AdventCalendar.js
@@ -1,18 +1,57 @@
 ﻿$(document).ready(function () {
     $('.picture').hide();
+
     $('#closeButton').click(function () {
         DefaultScreen();
         $('#popupPicture').hide();
     });
+
     $('#defaultScreenButton').click(function () {
         DefaultScreen();
     });
+
     $('#maxScreenButton').click(function () {
-        MaxScreen(); 
+        MaxScreen();
     });
+
     $(document).keydown(function () {
         KeyDown();
     });
+
+    $("#previousArea").hover(function () {
+        $('#previousButton').addClass("hover");
+    }, function () {
+        $('#previousButton').removeClass("hover");
+    });
+
+    $("#nextArea").hover(function () {
+        $('#nextButton').addClass("hover");
+    }, function () {
+        $('#nextButton').removeClass("hover");
+    });
+
+    $("#nextArea").click(function () {
+        if (NextPicture != null) {
+            Popup_ShowPictures_HideBox(NextPicture, NextBox);
+        }
+    });
+
+    $("#previousArea").click(function () {
+        if (PreviousPicture != null) {
+            Popup_ShowPictures_HideBox(PreviousPicture, PreviousBox);
+        }
+    });
+
+    //$('#previousArea').onmousemove = function () {
+    //    OnMouseInArea(PreviousButton);
+    //}
+    //$('#previousArea').TouchStart = function () {
+    //    OnMouseInArea(PreviousButton);
+    //}
+    //$('#previousArea').TouchEnd = function () {
+    //    OnMouseOutArea(PreviousButton);
+    //}
+
 });
 
 var Popup = document.getElementById("popupPicture");
@@ -32,13 +71,49 @@ var XBegin;
 var XEnd;
 var XDiff;
 
+
+function HideButtonsCursor() {
+    $('#previousButton').hide();
+    $('#nextButton').hide();
+    $('#closeButton').hide();
+    $('#maxScreenButton').hide();
+    $('#defaultScreenButton').hide();
+    $('#previousArea').css('cursor', 'none');
+    $('#nextArea').css('cursor', 'none');
+    $('#popupPicture').css('cursor', 'none');
+}
+
+function ShowButtonsCursor() {
+    $('#previousArea').css('cursor', 'pointer');
+    $('#nextArea').css('cursor', 'pointer');
+    $('#popupPicture').css('cursor', 'default');
+    $('#closeButton').show();
+    if (PreviousPicture != null)
+        $('#previousButton').show();
+    if (NextPicture != null)
+        $('#nextButton').show();
+    if (IsMaxScreen)
+        $('#defaultScreenButton').show();
+    else
+        $('#maxScreenButton').show();
+    ScheduleHideButtonsCursor();
+}
+
+function ScheduleHideButtonsCursor() {
+    clearTimeout(TimeoutHideBoutonsCursor);
+    TimeoutHideBoutonsCursor = setTimeout(function () {
+        HideButtonsCursor();
+    }, 800);
+}
+
+
 function DefaultScreen() {
     $('#caption').show();
     $('#largeImageAndButtons').height('94%');
     $('#largeImageAndButtons').width('100%');
     $('#popupPicture').padding = '0.5%';
-    $('#popupPicture').width('99%');
     $('#popupPicture').height('99%');
+    $('#popupPicture').width('99%');
     $('#defaultScreenButton').hide();
     $('#maxScreenButton').show();
     IsMaxScreen = false;
@@ -103,5 +178,16 @@ function KeyDown() {
 function ShowPicture_HideBox(pictureToShowPermanently, boxToHidePermanently) {
     $(pictureToShowPermanently).show();
     $(boxToHidePermanently).hide();
-    
 }
+
+function HideButtonsCursor() {
+    $('#previousButton').hide();
+    $('#nextButton').hide();
+    $('#closeButton').hide();
+    $('#maxScreenButton').hide();
+    $('#defaultScreenButton').hide();
+    $('#popupPicture').css('cursor', 'none');
+    $('#previousArea').hide();
+    $('#nextArea').hide();
+}
+
