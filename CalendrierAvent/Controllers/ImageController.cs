2a2281ec4bab465eb400b68b7cf271bd9b34458f
newtom69 <thomas@vuille.fr>
2019-12-15 19:23:25 +0100

    reprise dev ok

---
+++ CalendrierAvent/Controllers/ImageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AdventCalendar.Controllers
+{
+    public class ImageController : ParentController
+    {
+        // GET: Picture
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CalendrierAvent/Controllers/ParentController.cs
@@ -0,0 +1,24 @@
+﻿using AdventCalendar.DAL;
+using AdventCalendar.Tools;
+using AdventCalendar.ViewModels;
+using OmniFW.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventCalendar.Controllers
+{
+    public class ParentController : Controller
+    {
+        public ParentController()
+        {
+            Data.CnxStringStatic = ConfigurationManager.AppSettings["ChaineCnx"];
+            //-- Définition de la base de données par défaut
+            Data.TypeCnxStatic = TypeConnexion.SqlServer;
+        }
+    }
+}
